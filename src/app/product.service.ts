d876d16884bb4b68b98b0a1a3fa179f06d9f5df6
Martin Edvardsen <Martin.Edvardsen@isobar.com>
2020-11-20 08:33:44 +0100

    added private access modifier

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,11 +4,11 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  _albumUrl: '../assets/album.json';
+  private _albumUrl: '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  getAlbums(id: number) {
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(res => res.json())
   }
 }
